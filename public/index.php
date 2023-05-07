024936c35ccee2971589ef38daf430e18fcc58a2
pedriin647 <leonelpedro443@gmail.com>
2023-05-07 20:08:05 -0300

    creating controller

--- public/index.php
+++ public/index.php
@@ -1,1 +1,19 @@
 <?php
+
+require "../bootstrap.php";
+
+use core\Controller;
+
+
+try{
+
+    $controller = new Controller;
+    $controller = $controller->load();
+    dd($controller);
+    
+}catch(\Exception $e){
+
+    dd($e->getMessage());
+}
+
+
